b9182c06ae8cef6e7c6ce7362cbad8cc36b5425f
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-03-02 14:20:06 -0500

    first 3 problems working

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.engine('hypatia', (filePath, options, callback) => {
     const rendered = content.toString()
       .replace('#title#', '<title>' + options.title + '</title>')
       .replace('#message#', '<h1>' + options.message + '</h1>')
-      .replace('#content#','<div>'+ options.content + '</div>' )
+      .replace('#content#','<h1>'+ options.content + '</h1>' )
       .replace('#image#', '<img src =' + options.image + '>')
     return callback(null, rendered)
   })
@@ -27,6 +27,15 @@ app.get('/greeting/:name', (req,res) => {
 
 app.get('/tip/:total/:tipPercentage', (req,res) => {
     res.render('template1', { title: 'Tip', message: 'Your tip is ', content: req.params.total * (req.params.tipPercentage)/100})
+})
+
+const magicRes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
+app.get('/magic/:question', (req,res) => {
+    res.render('template1', {title: 'Magic Ball', message: "Your Question is: " + req.params.question, content: "The answer is : " + magicRes[Math.floor(Math.random() * 20)]
+
+    })
 })
 
 app.listen(port,() => {
